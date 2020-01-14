71c3f75341a8ad7a6d585a54f5e419a1c941e177
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:44:20 +0800

    Update ot28.cpp

--- ot28.cpp
+++ ot28.cpp
@@ -2,6 +2,7 @@
 
 long long func(long long n)
 {
+	/* n!=n*(n-1)*(n-2)*......*1 */
 	if(n==0 || n==1) return 1;
 	else return n*func(n-1);
 }
@@ -9,6 +10,8 @@ int main()
 {
 	long long n;
 	while(scanf("%lld",&n)!=EOF){
+		
+		/* using recursive function to calculate */
 		printf("%lld\n",func(n));
 	}
 	return 0;
