73927ad22432b67b59216d87fc9f05f9fedb156c
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:29:38 +0800

    Update mm27.cpp

--- mm27.cpp
+++ mm27.cpp
@@ -4,11 +4,15 @@ int main()
 {
 	long long i, a, b, tmp, sum=0;
 	scanf("%lld %lld",&a,&b);
+	
+	/* let a always be the smaller one */
 	if(a>b){
 		tmp=a;
 		a=b;
 		b=tmp;
 	}
+	
+	/* start from a to b, add all numbers into sum */
 	for(i=a;i<=b;i++){
 		sum+=i;
 	}
