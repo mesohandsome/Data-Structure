709625e8f27c1b92864393574cc38e747d61643d
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:39:12 +0800

    Update mm103.cpp

--- mm103.cpp
+++ mm103.cpp
@@ -2,6 +2,7 @@
 
 long long fibo(long long n)
 {
+	/* fibonacci N[i] = N[i-1] + N[i-2], and if n=1 or n=0, return 1 and 0 */
 	if(n==1 || n==0) return n;
 	else return fibo(n-1)+fibo(n-2);
 }
@@ -9,6 +10,8 @@ int main()
 {
 	long long n;
 	while(scanf("%lld",&n)!=EOF){
+		
+		/* using recursive function fibo() to calculate */
 		printf("%lld\n",fibo(n));
 	}
 	return 0;
