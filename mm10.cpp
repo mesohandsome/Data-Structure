8e7f159987ee38e30f2b862ddae36642e11190f3
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:24:13 +0800

    Update mm10.cpp

--- mm10.cpp
+++ mm10.cpp
@@ -4,7 +4,11 @@ int main()
 {
 	double n, ans;
 	scanf("%lf",&n);
+	
+	/* Because directly using 9/5 its value will be 1 , so I store the value 9 and 5 in double type a and b */
 	double a=9, b=5;
+	
+	/* calculate */
 	ans=((n*(a/b))+32);
 	printf("%.1lf\n",ans);
 	return 0;
