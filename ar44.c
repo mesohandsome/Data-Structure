b7feb9e94a46d9dbcbd096dd34270d49e7e4534c
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:12:02 +0800

    Update ar44.c

--- ar44.c
+++ ar44.c
@@ -7,16 +7,21 @@ int main()
 	char line[1024];
 	while(fgets(line,1024,stdin)!=NULL){
 		flag=0;
+		
+		/* remove new line symbol */
 		len=strlen(line);
 		if(line[len-1]=='\n') line[len-1]='\0';
+		
 		len=strlen(line);
 		for(i=0;i<(len/2);i++){
+			
+			/* determine the output will be YES or NO */
 			if(line[i]!=line[len-1-i]){
 				printf("NO\n");
 				flag=1;
 				break;
 			}
-		}
+		}	
 		if(flag==0) printf("YES\n");
 	}
 }
