9ae6b6567c7bc6f46f7f9c327e7e632965c825bb
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:09:22 +0800

    Update ar03.c

--- ar03.c
+++ ar03.c
@@ -4,8 +4,12 @@ int main()
 {
 	int i;
 	long long num, sum=0;
+	
+	/* input 6 numbers */
 	for(i=0;i<6;i++){
 		scanf("%lld",&num);
+		
+		/* multiply itself three times and add to sum */
 		num=(num*num*num);
 		sum+=num;
 	}
