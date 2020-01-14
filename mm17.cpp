79cbd8573cfe5c21d5b0951799f0a03357b2c04e
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:25:40 +0800

    Update mm17.cpp

--- mm17.cpp
+++ mm17.cpp
@@ -3,6 +3,8 @@
 int gcd(int a, int b)
 {
 	int tmp;
+	
+	/* 輾轉相除法 */
 	while(a%b!=0){
 		tmp=b;
 		b=a%b;
@@ -14,6 +16,8 @@ int main()
 {
 	int a,b;
 	scanf("%d %d",&a,&b);
+	
+	/* using gcd() find the answer */
 	printf("%d\n",gcd(a,b));
 	return 0;
 }
