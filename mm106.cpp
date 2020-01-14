4655dc87424800e9a90015817a41ad57b7a88a3a
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:40:44 +0800

    Update mm106.cpp

--- mm106.cpp
+++ mm106.cpp
@@ -3,6 +3,8 @@
 int isprime(int n)
 {
 	int i;
+	
+	/* if n % i ==0 then return 0 because the number is not prime. */
 	for(i=2;i<n;i++){
 		if(n%i==0) return 0; 
 	}
@@ -12,7 +14,11 @@ int main()
 {
 	int i, n;
 	scanf("%d",&n);
+	
+	/* start from n to 1 */
 	for(i=n;i>=1;i--){
+		
+		/* if the number is prime, then output */
 		if(isprime(i)){
 			printf("%d\n",i);
 			break;
