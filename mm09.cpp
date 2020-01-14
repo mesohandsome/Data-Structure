c1edcf8b07a80b79a1e3f51d0ea9a4eef8194d94
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:22:31 +0800

    Update mm09.cpp

--- mm09.cpp
+++ mm09.cpp
@@ -4,7 +4,11 @@ int main()
 {
 	long long n;
 	scanf("%lld",&n);
+	
+	/* if n>31, then output */
 	if(n>31) printf("Value of more than 31\n");
+	
+	/* else using << to calculate 2^n */
 	else{
 		printf("%lld\n",1<<n);
 	}
