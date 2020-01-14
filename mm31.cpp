923c5f02acaa7ffc41b523e7591ade9694a91336
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:30:44 +0800

    Update mm31.cpp

--- mm31.cpp
+++ mm31.cpp
@@ -5,7 +5,9 @@ int main()
 	long long i, n, sum=0;
 	scanf("%lld",&n);
 	for(i=0;i<=n;i++){
+		/* using if to determine whether the number is suitable or not */
 		if(i%2==0 && i%3==0 && i%12!=0){
+			/* add the suitable number into sum */
 			sum+=i;
 		}
 	}
