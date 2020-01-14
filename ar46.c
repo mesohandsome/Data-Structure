c91b128fe2590950da7679a552e6468aae49e843
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:13:11 +0800

    Update ar46.c

--- ar46.c
+++ ar46.c
@@ -3,7 +3,11 @@
 int main()
 {
 	double n, sum=0;
+	
+	/* scan the numbers until end of file */
 	while(scanf("%lf",&n)!=EOF){
+		
+		/* multiply n itself two times and add into sum */
 		n=(n*n);
 		sum+=n;
 	}
