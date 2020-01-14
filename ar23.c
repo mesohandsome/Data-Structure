804ea102367de79846daa2aed7e599fd836503a1
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:08:13 +0800

    Update and rename ar023.c to ar23.c

--- ar23.c
+++ ar23.c
@@ -5,8 +5,14 @@ int main()
 {
 	int i=0;
 	char line1[100], line2[100];
+	
+	/* ipnut two strings */
 	scanf("%s",line1);
 	scanf("%s",line2);
+	
+	/* using strstr() to find whether line1 is in line2 or not */
 	if(strstr(line2,line1)==NULL) printf("NO\n");
 	else printf("YES\n");
+	
+	return 0;
 }
