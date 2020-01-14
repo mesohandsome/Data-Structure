719ae6be4c78e9fe9bbae9120ea64fcaec7a1349
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:27:17 +0800

    Update mm21.cpp

--- mm21.cpp
+++ mm21.cpp
@@ -2,6 +2,7 @@
 
 long long func(long long n)
 {
+	/* n!=n*(n-1)*(n-2)*.....*1 */
 	if(n==1 || n==0) return 1;
 	else return n*func(n-1);
 }
@@ -9,6 +10,8 @@ int main()
 {
 	long long n;
 	scanf("%lld",&n);
+	
+	/* using recursive function to calculate */
 	printf("%lld\n",func(n));
 	return 0;
 }
