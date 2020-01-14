2390d830fee39abf9cad4a6faeed78fd9ff8d76d
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:46:59 +0800

    Update st02.cpp

--- st02.cpp
+++ st02.cpp
@@ -8,14 +8,21 @@ int main()
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
 		scanf("%s",str);
+		
+		/* remove new line symbol */
 		len=strlen(str);
 		if(str[len-1]=='\n') str[len-1]='\0';
-		strlen(str);
+		
+		len=strlen(str);
 		for(j=0;j<(len/2);j++){
+			
+			/* swap str[j] and str[len-1-j] */
 			tmp=str[j];
 			str[j]=str[len-1-j];
 			str[len-1-j]=tmp;
 		}
+		
+		/* output the complete array */
 		printf("%s\n",str);
 	}
 	return 0;
