89102adf11c8c67f3b35a2c3d823d198343f3b8a
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:42:40 +0800

    Update mm117.cpp

--- mm117.cpp
+++ mm117.cpp
@@ -5,12 +5,19 @@ int main()
 	int i, n, tmp;
 	while(scanf("%d",&n)!=EOF){
 		int arr[n*2];
+		
+		/* initialize arr[] to 0 */
 		for(i=0;i<n*2;i++) arr[i]=0;
+		
+		/* arr[input number] will be set to 1 */
 		for(i=0;i<n-1;i++){
 			scanf("%d",&tmp);
 			arr[tmp]=1;
 		}
+		
 		for(i=1;i<=n;i++){
+			
+			/* if arr[i]==0, it shows that the array doesn't include the number */
 			if(arr[i]==0){
 				printf("%d\n",i);
 				break;
