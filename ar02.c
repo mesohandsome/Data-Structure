cc895a51fe370df64461c2b4d2d109ccf4269a7b
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:05:58 +0800

    Update ar02.c

--- ar02.c
+++ ar02.c
@@ -4,12 +4,17 @@ int main()
 {
 	long long n[6];
 	int i;
+	
+	/* input 6 numbers */
 	for(i=0;i<6;i++){
 		scanf("%lld",&n[i]);
 	}
+	
+	/* output numbers from the last one */
 	for(i=5;i>=0;i--){
 		if(i==0) printf("%lld\n",n[i]);
 		else printf("%lld ",n[i]);
 	}
+	
 	return 0;
 } 
