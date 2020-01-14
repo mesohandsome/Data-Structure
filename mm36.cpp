4bb74e25e0a9ed0d60eb3d5b1e1ee61a976516fd
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:31:41 +0800

    Update mm36.cpp

--- mm36.cpp
+++ mm36.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
+	
+	/* using if to find n is in which range */
 	if(n>=3 && n<=5) printf("Spring\n");
 	else if(n>=6 && n<=8) printf("Summer\n");
 	else if(n>=9 && n<=11) printf("Autumn\n");
