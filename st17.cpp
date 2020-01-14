f0e192e58670d0144bc6634f9000dda7942978b7
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:49:06 +0800

    Update st17.cpp

--- st17.cpp
+++ st17.cpp
@@ -6,10 +6,16 @@ int main()
 	int len, i, flag=0;
 	char str[1024];
 	scanf("%s",str);
+	
+	/* remove new line symbol */
 	len=strlen(str);
 	if(str[len-1]=='\n') str[len-1]='\0';
+	
 	len=strlen(str);
 	for(i=0;i<(len/2);i++){
+		
+		/* if the condition doesn't set, then output NO */
+		/* else output YES */
 		if(str[i]!=str[len-1-i]){
 			printf("NO\n");
 			flag=1;
