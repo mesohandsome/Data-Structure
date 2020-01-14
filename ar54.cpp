7aa4002dc4b12d41408f77e4b0ad421c387f0155
mesohandsome <59432355+mesohandsome@users.noreply.github.com>
2020-01-14 23:18:04 +0800

    Update ar54.cpp

--- ar54.cpp
+++ ar54.cpp
@@ -5,14 +5,22 @@ int main()
 	char c[2];
 	int i, n, count[26];
 	scanf("%d",&n);
+	
+	/* initail count[] to 0 */
 	for(i=0;i<26;i++){
 		count[i]=0;
 	}
+	
+	/* input all the characters */
 	for(i=0;i<n;i++){
 		scanf("%s",c);
+		
+		/* character a's ascii is 97, so substract 97 */
 		count[c[0]-97]++;
 	}
 	for(i=0;i<26;i++){
+		
+		/* if the character's count is 0, it won't output */
 		if(count[i]==0) continue;
 		else{
 			printf("%c %d\n",i+97,count[i]);
